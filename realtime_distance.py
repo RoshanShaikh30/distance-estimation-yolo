11a401a77e443a5c5a69c1b0e588e11654b74111
Roshan Shaikh <roshanrizwan1414@gmail.com>
2026-09-04 19:37:39 +0530

    Update realtime_distance.py

--- realtime_distance.py
+++ realtime_distance.py
@@ -1,14 +1,39 @@
+from ultralytics import YOLO
 import cv2
+import time
+
+model = YOLO("yolov8s.pt")
 
 cap = cv2.VideoCapture(0)
 
+prev_time = time.time()
+
 while True:
+
     ret, frame = cap.read()
 
     if not ret:
         break
 
-    cv2.imshow("Webcam Test", frame)
+    results = model(frame)
+
+    annotated = results[0].plot()
+
+    current_time = time.time()
+    fps = 1 / (current_time - prev_time)
+    prev_time = current_time
+
+    cv2.putText(
+        annotated,
+        f"FPS: {int(fps)}",
+        (20, 40),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 255, 0),
+        2
+    )
+
+    cv2.imshow("YOLOv8 Detection", annotated)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
